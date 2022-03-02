d383c4ca8fd9aa91738d738c1ccaa017e42f22e2
spcmnd <spcmndd@protonmail.com>
2022-03-02 13:25:30 +0100

    Add some arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,10 @@
-use std::net::AddrParseError;
 use std::net::IpAddr;
 use std::net::TcpStream;
 
-pub fn scan_port(ip: IpAddr, port: i32) {
+pub fn scan_port(ip: IpAddr, port: i32) -> Option<i32> {
   if let Ok(_stream) = TcpStream::connect(format!("{}:{}", ip, port)) {
-    println!("Port {} opened", port);
+    Some(port)
+  } else {
+    None
   }
-}
-
-pub struct Target {
-  pub ip: IpAddr,
-}
-
-pub fn handle_args(args: Vec<String>) -> Result<Target, AddrParseError> {
-  let ip = match args[1].clone().parse::<IpAddr>() {
-    Ok(ip) => ip,
-    Err(e) => return Err(e),
-  };
-
-  Ok(Target { ip })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
-use std::env;
-use std::process;
+use clap::Parser;
+use std::net::IpAddr;
 
 mod lib;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long)]
+    pub target: IpAddr,
+
+    #[clap(short, long)]
+    pub output: bool,
+
+    #[clap(short, long)]
+    pub quick: bool,
+
+    #[clap(short, long)]
+    pub condensed: bool,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    if let Ok(target) = lib::handle_args(args) {
-        for port in 1..65536 {
-            lib::scan_port(target.ip, port);
+    let args = Args::parse();
+    let target = args.target;
+    let quick = args.quick;
+    let condensed = args.condensed;
+
+    let port_max = if quick { 10000 } else { 65536 };
+
+    let mut opened_ports: Vec<String> = Vec::new();
+
+    for port in 1..port_max {
+        let result = lib::scan_port(target, port);
+
+        match result {
+            Some(r) => {
+                opened_ports.push(r.to_string());
+
+                if !condensed {
+                    println!("Port {} is opened", r);
+                }
+            }
+            None => (),
         }
-    } else {
-        println!("IP Address is not valid");
-        process::exit(1);
+    }
+
+    if condensed {
+        println!("{}", opened_ports.join(","));
     }
 }
