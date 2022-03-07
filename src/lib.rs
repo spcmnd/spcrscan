d6a2999c2868505c35181a11cab19a36cd0a9dd2
spcmnd <spcmndd@protonmail.com>
2022-03-07 12:22:37 +0100

    Refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,81 @@
+use clap::Parser;
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::net::IpAddr;
 use std::net::TcpStream;
 
-pub fn scan_port(ip: IpAddr, port: i32) -> Option<i32> {
+pub fn scan_port(ip: IpAddr, port: u32) -> Option<u32> {
   if let Ok(_stream) = TcpStream::connect(format!("{}:{}", ip, port)) {
     Some(port)
   } else {
     None
+  }
+}
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+  #[clap(short, long)]
+  pub target: IpAddr,
+
+  #[clap(short, long)]
+  pub output: Option<String>,
+
+  #[clap(short, long)]
+  pub quick: bool,
+
+  #[clap(short, long)]
+  pub condensed: bool,
+}
+
+pub fn scan(args: Args) {
+  let Args {
+    target,
+    quick,
+    condensed,
+    output,
+  } = args;
+  let port_max: u32 = if quick { 10000 } else { 65536 };
+  let mut opened_ports: Vec<String> = Vec::new();
+  for port in 1..port_max {
+    let result = scan_port(target, port);
+
+    match result {
+      Some(r) => {
+        opened_ports.push(r.to_string());
+      }
+      None => (),
+    }
+  }
+
+  log(
+    match condensed {
+      true => format!("{}", opened_ports.join(",")),
+      false => opened_ports
+        .into_iter()
+        .map(|o| format!("Port {} is opened", o))
+        .collect::<Vec<String>>()
+        .join("\n"),
+    },
+    match &output {
+      None => "",
+      Some(x) => x,
+    },
+  );
+}
+
+pub fn log(content: String, filename: &str) {
+  println!("{}", content);
+
+  if !filename.is_empty() {
+    let mut file = OpenOptions::new()
+      .create(true)
+      .append(true)
+      .open(filename)
+      .unwrap();
+
+    if let Err(e) = writeln!(file, "{}", content) {
+      eprintln!("Couldn't write to file: {}", e);
+    }
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,8 @@
 use clap::Parser;
-use std::net::IpAddr;
 
 mod lib;
-mod logger;
-
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-pub struct Args {
-    #[clap(short, long)]
-    pub target: IpAddr,
-
-    #[clap(short, long)]
-    pub output: Option<String>,
-
-    #[clap(short, long)]
-    pub quick: bool,
-
-    #[clap(short, long)]
-    pub condensed: bool,
-}
 
 fn main() {
-    let Args {
-        target,
-        quick,
-        condensed,
-        output,
-    } = Args::parse();
-
-    let port_max = if quick { 10000 } else { 65536 };
-    let mut opened_ports: Vec<String> = Vec::new();
-
-    for port in 1..port_max {
-        let result = lib::scan_port(target, port);
-
-        match result {
-            Some(r) => {
-                opened_ports.push(r.to_string());
-            }
-            None => (),
-        }
-    }
-
-    logger::log(
-        match condensed {
-            true => format!("{}", opened_ports.join(",")),
-            false => opened_ports
-                .into_iter()
-                .map(|o| format!("Port {} is opened", o))
-                .collect::<Vec<String>>()
-                .join("\n"),
-        },
-        match &output {
-            None => "",
-            Some(x) => x,
-        },
-    );
+    let args = lib::Args::parse();
+    lib::scan(args);
 }
